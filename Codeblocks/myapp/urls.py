a408c23640887c6c29dcca2d92387d8d2fbb18e4
Tintu Samson <85018591+tintu-samson@users.noreply.github.com>
2023-05-22 21:19:32 +0530

    added signup and login page with authentication

--- Codeblocks/myapp/urls.py
+++ Codeblocks/myapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='home'),
+    
+    
+    path('signup',views.signup,name='signup'),
+    path('login',views.login_user,name='login'),
+    path('logout',views.logout_user,name='logout'),
     
 ]
--- Codeblocks/myapp/views.py
+++ Codeblocks/myapp/views.py
@@ -1,9 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Domain,Events
+from .forms import createUserForm
+from .decorators import unauthenticated_user
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
 
 def index(request):
     domain = Domain.objects.all()
     events = Events.objects.all()
     context = {'domain':domain,'events':events}
     return render(request,"myapp/index.html",context)
+
+@unauthenticated_user
+def signup(request): 
+    form = createUserForm()
+    if request.method == 'POST':
+        form = createUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request,"myapp/signup.html",context)
+
+@unauthenticated_user
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        
+        else:
+            messages.info(request,"Username or Password is incorrect")
+            return redirect('login')
+    
+    return render(request,"myapp/login.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
